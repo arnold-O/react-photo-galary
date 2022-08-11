ce994693093b109f7f549446ab2838d1ae4d02e9
arnold-oghiator <arnoldoghiator@gmail.com>
2022-08-11 23:44:47 +0100

    photo fetch setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+
+const mainUrl = `https://api.unsplash.com/photos/`;
 
 function App() {
+  const FetchData = async () => {
+    let url;
+    url = `${mainUrl}?client_id=ImGoC-ocaNY2EWT7blSawFHIWiqomdoy7c7I-Hn2unM`;
+
+    try {
+      const response = await fetch(url);
+      const res = response.json();
+      console.log(res);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    FetchData();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container-wrapper">
+      <form className="search-form-wrapper">
+        <input type="text" className="input-search" placeholder="Search Photo" />
+        <button className="search-Btn">Serach</button>
+      </form>
     </div>
   );
 }
